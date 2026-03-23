d8297a7ce727152373160f2878d91033df21ab36
amishko <andreimishko084934@gmail.com>
2026-03-23 18:12:17 +0100

    fixed tagId Null

--- src/main/java/com/saveit/bff/notes/feign/dto/NoteServiceRequestDto.java
+++ src/main/java/com/saveit/bff/notes/feign/dto/NoteServiceRequestDto.java
@@ -1,15 +1,20 @@
 package com.saveit.bff.notes.feign.dto;
 
+import com.saveit.bff.notes.validation.ValidTags;
 import com.saveit.bff.notes.web.dto.NotePriority;
 import com.saveit.bff.notes.web.dto.NoteSource;
 import com.saveit.bff.notes.web.dto.NoteStatus;
 import com.saveit.bff.notes.web.dto.TagDto;
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Builder;
 
 import java.util.Set;
 
 @Builder(toBuilder = true)
-public record NoteServiceRequestDto(@NotBlank String noteId, @NotBlank String userId, String title, @NotBlank String content,
-                                    @NotBlank NoteSource source, @NotBlank NoteStatus status, @NotBlank NotePriority priority,
-                                    Set<TagDto> tags) {}
+public record NoteServiceRequestDto(@NotBlank String noteId, @NotBlank String userId, String title,
+                                    @NotBlank String content,
+                                    @NotBlank NoteSource source, @NotBlank NoteStatus status,
+                                    @NotBlank NotePriority priority,
+                                    @Valid @ValidTags Set<TagDto> tags) {
+}
--- src/main/java/com/saveit/bff/notes/web/dto/NoteRequestDto.java
+++ src/main/java/com/saveit/bff/notes/web/dto/NoteRequestDto.java
@@ -1,5 +1,6 @@
 package com.saveit.bff.notes.web.dto;
 
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Builder;
 
@@ -7,5 +8,5 @@ import java.util.Set;
 
 @Builder(toBuilder = true)
 public record NoteRequestDto(String noteId, @NotBlank String userId, String title, NoteStatus status,
-                             @NotBlank String content, NotePriority priority, Set<TagDto> tags) {
+                             @NotBlank String content, NotePriority priority, @Valid Set<TagDto> tags) {
 }
